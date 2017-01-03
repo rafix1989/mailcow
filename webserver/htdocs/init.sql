e8eb7712ae799038146e5fd50a71e34f2e64f899
rafix1989 <rafix1989@interia.pl>
2017-01-03 12:19:07 +0100

    Update init.sql
    Change charset in database to UTF-8

--- webserver/htdocs/init.sql
+++ webserver/htdocs/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `modified` datetime NOT NULL DEFAULT '2016-01-01 00:00:00',
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `alias` (
   `address` varchar(255) NOT NULL,
@@ -17,19 +17,19 @@ CREATE TABLE IF NOT EXISTS `alias` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`address`),
   KEY `domain` (`domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `sender_acl` (
   `logged_in_as` varchar(255) NOT NULL,
   `send_as` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `spamalias` (
   `address` varchar(255) NOT NULL,
   `goto` text NOT NULL,
   `validity` int(11) NOT NULL,
   PRIMARY KEY (`address`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `alias_domain` (
   `alias_domain` varchar(255) NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `alias_domain` (
   PRIMARY KEY (`alias_domain`),
   KEY `active` (`active`),
   KEY `target_domain` (`target_domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `domain` (
   `domain` varchar(255) NOT NULL,
@@ -51,12 +51,12 @@ CREATE TABLE IF NOT EXISTS `domain` (
   `quota` bigint(20) NOT NULL DEFAULT '0',
   `transport` varchar(255) NOT NULL,
   `backupmx` tinyint(1) NOT NULL DEFAULT '0',
-  `relay_all_recipients` tinyint(1) NOT NULL DEFAULT '0',
+u  `relay_all_recipients` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime NOT NULL DEFAULT '2016-01-01 00:00:00',
   `modified` datetime NOT NULL DEFAULT '2016-01-01 00:00:00',
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `domain_admins` (
   `username` varchar(255) NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `domain_admins` (
   `created` datetime NOT NULL DEFAULT '2016-01-01 00:00:00',
   `active` tinyint(1) NOT NULL DEFAULT '1',
   KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `mailbox` (
   `username` varchar(255) NOT NULL,
@@ -81,14 +81,14 @@ CREATE TABLE IF NOT EXISTS `mailbox` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`),
   KEY `domain` (`domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `quota2` (
   `username` varchar(100) NOT NULL,
   `bytes` bigint(20) NOT NULL DEFAULT '0',
   `messages` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `fugluconfig` (
   `scope` varchar(255) NOT NULL,
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `fugluconfig` (
   `option` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   PRIMARY KEY (`scope`,`section`,`option`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `userpref` (
   `username` varchar(100) NOT NULL DEFAULT '',
@@ -105,4 +105,4 @@ CREATE TABLE IF NOT EXISTS `userpref` (
   `prefid` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`prefid`),
   KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
